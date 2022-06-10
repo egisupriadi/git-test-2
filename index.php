83ca0a316126206876cb4489073bedfa4ea444dd
egisupriadi <egi.supriadi.334@gmail.com>
2022-06-10 15:42:32 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam cum expedita beatae, dolore fuga eveniet obcaecati. Soluta ea commodi nisi ex officiis! Quae sapiente praesentium ex exercitationem dolores laudantium?</p>
 </body>
 </html>
